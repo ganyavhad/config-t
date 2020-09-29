088f0e578786af495b7cd5a6f67109befc34cfa2
ganyavhad <36654727+ganyavhad@users.noreply.github.com>
2020-09-29 09:27:03 +0530

    get route added

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 const config = require("config");
-console.log("config==============", config.get("keyName"));
+console.log("config==============", config.get("keyName"));.
+
+app.get("/", (req, res, next) => {
+    res.send("Server works")
+})
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Listening on ${process.env.PORT?process.env.PORT:3000}`);
 });
