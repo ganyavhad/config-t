4abed244b51cb61d24598db6fb06a9b8ee678a68
ganyavhad <36654727+ganyavhad@users.noreply.github.com>
2020-09-29 09:45:23 +0530

    changes

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const config = require("config");
+process.env.NODE_ENV = 'production'
 console.log("config==============", config.get("keyName"));
-
+console.log(process.env.NODE_ENV)
 app.get("/", (req, res, next) => {
     res.send("Server works")
 })
